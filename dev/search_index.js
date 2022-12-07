2476fa30f01257dffbe384191469c2c0a90d9d10
Documenter.jl <documenter@juliadocs.github.io>
2022-12-07 18:22:29 +0000

    build based on 29234d9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SchottkyanoMaLy","category":"page"},{"location":"#SchottkyanoMaLy","page":"Home","title":"SchottkyanoMaLy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SchottkyanoMaLy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SchottkyanoMaLy]","category":"page"}]
+}
