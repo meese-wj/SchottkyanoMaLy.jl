ae9c3079cd9ab944f811329bbade511e9a3e1466
W. Joe Meese <meese022@umn.edu>
2022-12-07 18:13:20 -0600

    Add documentation

--- src/DonutVolcanoEnsembles.jl
+++ src/DonutVolcanoEnsembles.jl
@@ -1,5 +1,11 @@
 
 using SpecialFunctions
+import Base: eltype, push!, append!
+
+# Base overloads
+export eltype, push!, append!
+# DonutVolcanoEnsemble stuff
+export donutvolcano, DonutVolcanoEnsemble
 
 @doc raw"""
     Theta(x)
@@ -50,4 +56,96 @@ The explicit form is given by
 function norm_donutvolcano(μ, σ)
 	arg = μ / (σ * sqrt(2))
 	return σ^2 * exp( -arg^2 ) + sqrt(π/2) * μ * σ * (oneunit(μ) + erf(arg))
+end
+
+@doc raw"""
+    DonutVolcanoEnsemble{T <: AbstractFloat}
+
+An ensemble of ``M`` [`donutvolcano`](@ref)s takes the form 
+
+```math
+\mathrm{p}_M(x) = \frac{1}{M} \sum_{k=1}^M \mathrm{donutvolcano}(x, \mu_k, \sigma_k).
+```
+
+This object is a wrapper around an `ensemble::Vector{Tuple{T, T}}` for the individual
+[`donutvolcano`](@ref)s.
+"""
+struct DonutVolcanoEnsemble{T <: AbstractFloat}
+	ensemble::Vector{Tuple{T, T}}
+end
+
+# Constructors
+"""
+    DonutVolcanoEnsemble(args...)
+
+Various interfaces to a [`DonutVolcanoEnsemble`](@ref)
+
+# Copy/Move constructors
+
+* `DonutVolcanoEnsemble(::Vector{Tuple{T, T}})`
+    * Create an ensemble from a `Vector` of `(μ, σ)` `Tuple`s of the same `Type`.
+
+* `DonutVolcanoEnsemble(::Vector{Tuple{T, U}})`
+    * Create an ensemble from a `Vector` of `(μ, σ)` `Tuple`s of the different `Type`s. 
+    * The type of the ensemble is chosen by `Base.promote_type`.
+
+* `DonutVolcanoEnsemble(::Type{S}, ::Vector{Tuple{T, U}})`
+    * Create an ensemble from a `Vector` of `(μ, σ)` `Tuple`s of the different `Type`s into an ensemble of with preferred type `S`.
+    * The ultimate type of the ensemble is chosen by `Base.promote_type`.
+    
+# Empty constructors
+
+* `DonutVolcanoEnsemble(::Type{S})`
+    * Create an empty ensemble of chosen type `S`.
+
+* `DonutVolcanoEnsemble()`
+    * Create an empty ensemble. By default the type is `Float64`.
+"""
+DonutVolcanoEnsemble( tups::Vector{Tuple{T, T}} ) where T = DonutVolcanoEnsemble{T}( tups )	
+function DonutVolcanoEnsemble(tups::Vector{Tuple{T, U}}) where {T, U}
+	t = promote_type(T, U)
+	new_tups = map( tup -> convert.(t, tup), tups )
+	return DonutVolcanoEnsemble(new_tups)
+end
+function DonutVolcanoEnsemble(::Type{S}, tups::Vector{Tuple{T, U}}) where {S, T, U}
+	dve = DonutVolcanoEnsemble(tups)
+	t = promote_type( eltype(dve), S )
+	return DonutVolcanoEnsemble( map( tup -> convert.(t, tup), ensemble(dve) ) )
+end
+## Empty constructors
+DonutVolcanoEnsemble(::Type{T}) where T = DonutVolcanoEnsemble{T}( Tuple{T, T}[] )
+DonutVolcanoEnsemble() = DonutVolcanoEnsemble(Float64)
+
+# Getters
+"""
+    ensemble(::DonutVolcanoEnsemble)
+
+Getter for the `Vector{Tuple{T, T}}` of the given ensemble.
+"""
+ensemble(dve::DonutVolcanoEnsemble) = dve.ensemble
+"""
+    get_pair(::DonutVolcanoEnsemble, pair_idx) -> Tuple{T, T}
+
+Getter for a single pair (`Tuple{T, T}`) at a specified `pair_idx`.
+"""
+get_pair(dve::DonutVolcanoEnsemble, pair_idx) = Tuple( ensemble(dve)[pair_idx] )
+"""
+    npairs(::DonutVolcanoEnsemble)
+
+Return the number of [`donutvolcano`](@ref) in the [`DonutVolcanoEnsemble`](@ref).
+"""
+npairs(dve::DonutVolcanoEnsemble) = length(ensemble(dve))
+
+# Convenient Base overloads
+Base.eltype(::DonutVolcanoEnsemble{T}) where T = T
+Base.push!(dve::DonutVolcanoEnsemble{T}, pair) where T = push!(ensemble(dve), convert.(T, pair))
+Base.append!(dve::DonutVolcanoEnsemble{T}, vals) where T = append!(ensemble(dve), map(x -> convert.(T, x), vals))
+
+# Make the DonutVolcanoEnsemble a callable function over a set of values x
+function (dve::DonutVolcanoEnsemble)(x)
+	output = zero.(x)
+	@inbounds for pdx ∈ UnitRange(1, npairs(dve))
+		output += donutvolcano(x, get_pair(dve, pdx)...)
+	end
+	return output ./ npairs(dve)
 end
