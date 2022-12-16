0b4c02e6cf1007dc72f4b6e6e1a2f7d4db4e1fc5
W. Joe Meese <meese022@umn.edu>
2022-12-16 12:24:00 -0600

    Add Cheby decompositions

--- test/test_cVLA.jl
+++ test/test_cVLA.jl
@@ -12,6 +12,16 @@ using SchottkyAnoMaLy
         temps = zeros(4)
         mat = zeros(6, 6)
         @test_throws AssertionError populate_msqdiffs!(mat, cV_arr, temps)
+
+        all_ensembles = rand(RandomDonutVolcanoGenerator(50, 10, 10))
+        num_ens = length(all_ensembles)
+        order = 22
+        coeffs = zeros( num_ens == 2 ? 3 : 2, order + 1 )
+        @test_throws AssertionError chebycoefficients!(coeffs, all_ensembles, order, 0, 100)
+        
+        coeffs = zeros( num_ens, order )
+        @test_throws AssertionError chebycoefficients!(coeffs, all_ensembles, order, 0, 100)
+        
     end
 
 end
