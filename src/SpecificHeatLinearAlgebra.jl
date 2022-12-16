bd616c2f3c1ab9bb198f1b50aa76f6cafc5c622c
W. Joe Meese <meese022@umn.edu>
2022-12-16 17:01:57 -0600

    Add code-reuse to compute the kernel column

--- src/SpecificHeatLinearAlgebra.jl
+++ src/SpecificHeatLinearAlgebra.jl
@@ -4,6 +4,7 @@ using NumericalIntegration
 using FastChebInterp
 
 export compute_cVs!, compute_cVs,
+       input_test_msqdiffs, input_test_msqdiffs!,
        populate_msqdiffs!, populate_msqdiffs,
        kernel, 
        chebycoefficients!, chebycoefficients
@@ -44,6 +45,30 @@ function compute_cVs(ensembles, temps; kwargs...)
     return compute_cVs!(cV_arr, ensembles, temps; kwargs...)
 end
 """
+    input_test_msqdiffs!(output, test_cV, cV_arr, temps, [method = TrapezoidalFast()])
+
+Compute a single column vector of the [`msqdiff`](@ref)s between the `test_cV`
+argument and each column of the `cV_arr`, all measured as functions of `temps` 
+temperature. Store the result in the `output` vector.
+"""
+function input_test_msqdiffs!(output, test_cV, cV_arr, temps; method = TrapezoidalFast())
+    @assert length(test_cV) == size(cV_arr)[1] == length(temps) "Size mismatch. Each specific heat must have the same number of temperatures ($(length(temps))). Got $(length(test_cV)) and $(size(cV_arr)[1])."
+    @assert length(output) == size(cV_arr)[2] "Size mismatch. The length of the output vector must be the number of ensembles. Got $(length(output)) and $(size(cV_arr)[2])."
+    @inbounds @simd for idx ∈ eachindex(output)
+        output[idx] = msqdiff(temps, view(cV_arr, :, idx), test_cV, method)
+    end
+    return output
+end
+"""
+    input_test_msqdiffs(test_cV, cV_arr, temps, [method = TrapezoidalFast()])
+
+Allocate an appropriate `output` `Vector` and pass it to [`input_test_msqdiffs!`](@ref).
+"""
+function input_test_msqdiffs(test_cV, cV_arr, temps; method = TrapezoidalFast())
+    output = zeros(eltype(test_cV), size(cV_arr)[2])
+    return input_test_msqdiffs!(output, test_cV, cV_arr, temps; method = method)
+end
+"""
     populate_msqdiffs!( msqdiff_mat, cV_arr, temps; [kwargs...])
 
 Compute the [`msqdiff`](@ref) for each pair of specific heats in `cV_arr` as 
@@ -53,27 +78,33 @@ a function of temperature (`temps`) as the `xdata`. This assumes that
 2. `N == size(cV_arr)[2]`, where `cV_arr` is a specific heat `Matrix` calculated from [`compute_cVs!`](@ref),
 3. the number of temperatures in `temps` equals the number of specific heat rows.
 
-This populates the `msqdiff_mat` by exploiting the symmetry in [`msqdiff`](@ref). So 
-only `N(N+1)/2` elements are computed and then the matrix is symmetrized. (Hence the need
-for a zero matrix input.)
+This function uses [`input_test_msqdiffs!`](@ref) to fill the `Matrix`. The `input` is the 
+specific heat of a given ensemble, and the `test`s are those from all other ensembles.
 
 !!! note
-    `N` is likely to be a large number, so it is best to use this function once.
+`N` is likely to be a large number, so it is best to use this function once.
 """
 function populate_msqdiffs!( msqdiff_mat, cV_arr, temps; method = TrapezoidalFast() )
+    # Not true anymore...
+    # This populates the `msqdiff_mat` by exploiting the symmetry in [`msqdiff`](@ref). So 
+    # only `N(N+1)/2` elements are computed and then the matrix is symmetrized. (Hence the need
+    # for a zero matrix input.)
     @assert size(cV_arr)[1] == length(temps) "Size mismatch. There must be as many temperatures ($(length(temps))) as specific heats ($(size(cV_arr)[1]))."
     @assert size(msqdiff_mat)[1] == size(msqdiff_mat)[2] == size(cV_arr)[2] "Size mismatch. The first argument must be a Matrix of size N × N, where N is the number of columns in the second argument."
     
     NL = size(msqdiff_mat)[1]
     @inbounds Threads.@threads for col ∈ UnitRange(1, NL)
         ycol = @view cV_arr[:, col]
-        msqdiff_mat[col, col] = 0.5 * msqdiff( temps, ycol, ycol, method ) # half because it will be added back in the transpose
-        @simd for row ∈ UnitRange(col + oneunit(col), NL)
-            yrow = @view cV_arr[:, row]
-            msqdiff_mat[row, col] = msqdiff(temps, yrow, ycol, method)
-        end
+        input_test_msqdiffs!(view(msqdiff_mat, :, col), ycol, cV_arr, temps; method = method)
     end
-    msqdiff_mat .= msqdiff_mat .+ transpose(msqdiff_mat)
+    # TODO: Should I keep this method or rely on input_test_msqdiffs?
+    #     msqdiff_mat[col, col] = 0.5 * msqdiff( temps, ycol, ycol, method ) # half because it will be added back in the transpose
+    #     @simd for row ∈ UnitRange(col + oneunit(col), NL)
+    #         yrow = @view cV_arr[:, row]
+    #         msqdiff_mat[row, col] = msqdiff(temps, yrow, ycol, method)
+    #     end
+    # end
+    # msqdiff_mat .= msqdiff_mat .+ transpose(msqdiff_mat)
     return msqdiff_mat
 end
 """
--- test/test_cVLA.jl
+++ test/test_cVLA.jl
@@ -5,6 +5,7 @@ using SchottkyAnoMaLy
 @time @testset "Testing SpecificHeatLinearAlgebra" begin
     
     @testset "AssertionErrors" begin
+        # Test populate_msqdiffs
         cV_arr = zeros(4, 5)
         temps = zeros(3)
         @test_throws AssertionError populate_msqdiffs(cV_arr, temps)
@@ -13,6 +14,19 @@ using SchottkyAnoMaLy
         mat = zeros(6, 6)
         @test_throws AssertionError populate_msqdiffs!(mat, cV_arr, temps)
 
+        # Test input_test_msqdiffs
+        temps = zeros(10)
+        cV_arr = zeros(9, 9)
+        test_cV = zeros(9)
+        @test_throws AssertionError input_test_msqdiffs(test_cV, cV_arr, temps)
+
+        cV_arr = zeros(10, 9)
+        @test_throws AssertionError input_test_msqdiffs(test_cV, cV_arr, temps)
+        
+        output = zeros(9)
+        @test_throws AssertionError input_test_msqdiffs!(output, test_cV, cV_arr, temps)
+
+        # Test chebycoefficients
         all_ensembles = rand(RandomDonutVolcanoGenerator(50, 10, 10))
         num_ens = length(all_ensembles)
         order = 22
