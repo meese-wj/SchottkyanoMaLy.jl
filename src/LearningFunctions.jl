7117bf1b9a94647ca242e48439a53e9ecef8df0f
W. Joe Meese <meese022@umn.edu>
2022-12-31 15:03:36 -0600

    Found bug in gradient. GradientDescent still broken

--- src/LearningFunctions.jl
+++ src/LearningFunctions.jl
@@ -61,9 +61,9 @@ function single_component_loss_gradient( prediction::AbstractFloat, value::Abstr
     Minv_f_vector = Minv * fcomp_vector # column vector
     νT_Minv = transpose(νvector) * Minv # row vector
     ∂σval  = dot( ∂σνvector, Minv_f_vector) # column ⋅ column
-    ∂σval += dot( νT_Minv, Qmat, Minv_f_vector) # column ⋅ matrix ⋅ column
-    ∂λval = dot( νvector, Minv, Minv_f_vector ) # column ⋅ column
-    return single_component_deviation(prediction, value) .* ( -∂σval, -∂λval ) # return a Tuple as the gradient ∇ = (∂σ, ∂λ)
+    ∂σval += -dot( νT_Minv, Qmat, Minv_f_vector) # column ⋅ matrix ⋅ column
+    ∂λval = -dot( νvector, Minv, Minv_f_vector ) # column ⋅ column
+    return single_component_deviation(prediction, value) .* ( ∂σval, ∂λval ) # return a Tuple as the gradient ∇ = (∂σ, ∂λ)
 end
 function single_component_loss_gradient( component_idx::Int, ensemble_idx::Int, updated_trainset, updated_interpset )
     pred = predicted_components(updated_trainset)[component_idx, ensemble_idx]
